641280d6f09da8981fb5ddb83532bfb6c777b886
Stone Shi <stone@MacBook-Pro-10.local>
2019-08-27 11:32:28 +0800

    Add file minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
-// const exec = require('gulp-exec');
+const { src, dest } = require('gulp');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 
-function minify (cb) {
-  cb();
+function minify () {
+  return src('build/**/*.js')
+    .pipe(uglify())
+    .pipe(concat('free-draw.min.js'))
+    .pipe(dest('dist/'));
 }
 
 exports.minify = minify;
